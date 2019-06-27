7909394bd53e8793e31788adf74002394260372d
stfhopi <stfzoltan@gmail.com>
2019-06-27 16:54:24 +0200

    remaining secs

---
+++ week1/day04-Js-Typescrpt/currentSec.ts
@@ -0,0 +1,7 @@
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+
+console.log(
+  86400 - (3600 * currentHours + 60 * currentMinutes + currentSeconds)
+);
