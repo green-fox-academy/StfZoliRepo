9b11d0a2ea356da36c42d2d9253f4722a21b71de
stfhopi <stfzoltan@gmail.com>
2019-06-27 14:44:21 +0200

    basicNfoAdded

---
+++ week1/day04-Js-Typescrpt/basicNfo.ts
@@ -0,0 +1,11 @@
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+let myname: string = "Z";
+let myage: number = 32;
+let myheight: number = 1.78;
+let married: boolean = false;
+
+console.log(myname, myage, myheight, married);
