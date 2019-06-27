7a73986ff6db089290a3e7db06a545ed8907ea36
stfhopi <stfzoltan@gmail.com>
2019-06-27 10:53:16 +0200

    pratice

---
+++ week1/day04-Js-Typescrpt/ifStatements.ts
@@ -0,0 +1,10 @@
+let temperature: number = 35;
+if (temperature > 30) {
+  console.log("its just perfect!");
+}
+if (temperature < 10) {
+  console.log("its freaking cold");
+}
+if ((true && true) || (false && false)) {
+  console.log("wtf?");
+}
