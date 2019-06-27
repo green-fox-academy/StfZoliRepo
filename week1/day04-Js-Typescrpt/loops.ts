7cc0024336cac80dcc021f30271737484622fcfb
stfhopi <stfzoltan@gmail.com>
2019-06-27 11:26:29 +0200

    blah

---
+++ week1/day04-Js-Typescrpt/loops.ts
@@ -0,0 +1,9 @@
+let times: number = 0;
+
+while (times < 10) {
+  console.log("blah", times);
+  times++;
+}
+for (let iterator: number = 0; iterator < 100; iterator++) {
+  console.log(iterator);
+}
