ce87c9e166db6bb19f0cc7abec3e556da48eff48
stfhopi <stfzoltan@gmail.com>
2019-06-28 11:40:37 +0200

    even loop yeah

---
+++ week1/day05-Js-typescript/printeven.ts
@@ -0,0 +1,16 @@
+export {};
+// Create a program that prints all the even numbers between 0 and 500
+
+/*let a: number = 0;
+while (a < 501) {
+  if (a % 2 == 0) {
+    console.log(a);
+  }
+  a++;
+}
+*/
+for (let i: number = 0; i < 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i); // Prints the numbers from 0 to 99
+  }
+}
