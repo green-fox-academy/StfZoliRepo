a63a394c1333c731c47ff2f101b3bf4a20102f79
stfhopi <stfzoltan@gmail.com>
2019-07-01 11:48:17 +0200

    recap,mattreview2

--- week02/day01-functions-array/sum.ts
+++ week02/day01-functions-array/sum.ts
@@ -18,3 +18,22 @@ let diffArray: [string, boolean] = ["aName", true];
 let diffArray2: Array<string | number> = ["nameAgain", 3, 11];
 
 console.log(diffArray2);
+diffArray.splice(0, 1, "z");
+
+let doubled: number[] = [1, 2, 3, 4, 5].map(function(i) {
+  return i * 2;
+});
+console.log(doubled);
+let odds: number[] = [1, 2, 3, 4, 5].filter(function(item) {
+  return item % 2 == 1;
+});
+console.log(odds);
+
+//spread
+
+let array1: number[] = [1, 2];
+let array2: number[] = [3, 4];
+
+let array3: number[] = [...array1, ...array2];
+
+console.log(array3);
