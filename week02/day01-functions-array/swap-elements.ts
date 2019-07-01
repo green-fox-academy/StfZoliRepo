266c90014557a389bd60ba82b99ec74268eef4de
stfhopi <stfzoltan@gmail.com>
2019-07-01 14:50:37 +0200

    swapping elements

---
+++ week02/day01-functions-array/swap-elements.ts
@@ -0,0 +1,7 @@
+"use strict";
+
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc: string[] = ["Arthur", "Boe", "Chloe"];
+console.log([abc[2], abc[1], abc[0]]);
